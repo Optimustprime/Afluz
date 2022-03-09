74180707d0396f0705e7b1bd8d1f41201ddfc659
Optimustprime <73552453+Optimustprime@users.noreply.github.com>
2022-03-09 15:57:11 +0100

    Add files via upload

---
+++ Afluz.js
@@ -0,0 +1,19 @@
+//////// javascript source code
+////function correct() {
+////    var x = Math.round($("#adjust").outerWidth() / $('#adjust').parent().width() * 100);
+////    alert(x);
+
+////    if (x = 33) {
+////        $("#div4").css("margin-left", "20%")
+////    }
+////    alert(x);
+
+////    else {
+////        var x = Math.round($("#adjust").outerWidth() / $('#adjust').parent().width() * 100);
+
+////        $("#div4").css("margin-left", "5%")
+////    }
+////    alert(x);
+
+////}
+////correct();
